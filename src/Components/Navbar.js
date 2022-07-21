a0701d45aa90af23d57398b161dc29cf8c44c6ec
Ratnadeep Ghosh <ratnadeepghosh36@gmail.comm>
2022-07-21 13:15:55 +0530

    :fire: axios,lifecycle method

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
   render() {
     return (
-        <div style={{display:'flex', background:"#131A27", padding:'1rem', justifyContent:"center", alignItems:"center"}}>
-            <h1 style={{color:"white"}}>Movies App</h1>
-            <h2 style={{marginLeft:"2rem",color:"white"}}>Favourites</h2>
+        <div style={{display:'flex', background:"white", color:"blue",padding:'1rem', justifyContent:"center", alignItems:"center"}}>
+            <h1>Movies App</h1>
+            <h2 style={{marginLeft:"2rem",}}>Favourites</h2>
       </div>
     )
   }
